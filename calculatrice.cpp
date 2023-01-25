ca9ef0da7b6120e194933cad7ad85086766a1f8f
cath-fmarie <sabrinaallouch@hotmail.com>
2023-01-25 01:33:26 +0100

    loop, exit

--- calculatrice.cpp
+++ calculatrice.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-#include <math.h>
+#include <stdlib.h>
 #include <windows.h>
+#include <typeinfo>
 
 using namespace std;
 
@@ -14,11 +15,13 @@ SetConsoleOutputCP(65001); // codage utf8
 float num1, num2;
 char operateur;
 
+
 cout<< "=========== CALCULATRICE ============\n\
 +++++++++++++++++++++++++++++++++++++\n";
 
+while (1){
 
-cin >>num1>>operateur>>num2;
+cin >>num1>>operateur>>num2;}
 
 switch(operateur){
     case '+':
@@ -36,8 +39,9 @@ switch(operateur){
         break;
         // cas puissance et racine carrée et modulo
     default :
-        cout<<"Opération invalide"<<endl;
-        
+        cout<<"Opération invalide"<<endl; 
+        exit(1);
+    }     
 }
 
 return 0;
