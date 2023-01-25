eb5b958c25be4d0a53aca49f62a8f03f4b2d784b
cath-fmarie <sabrinaallouch@hotmail.com>
2023-01-25 02:13:39 +0100

    menu

---
+++ ATM.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <stdlib.h>
+#include <windows.h>
+
+using namespace std;
+
+
+
+void menu(){
+    cout<<"==================================="<<endl;
+    cout<<"************PICSOU BANK************\n";
+    cout<<"==================================="<<endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main(){
+SetConsoleOutputCP(65001); // codage utf8 
+
+
+
+menu();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
